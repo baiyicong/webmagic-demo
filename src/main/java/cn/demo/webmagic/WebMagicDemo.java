57c383aca595eaebd8bfe537043540ffe717cff5
baiyicong <baiyicongwb@vivooutsourceuser.com>
2018-12-10 09:54:57 +0800

    启动项方法名的更改

--- src/main/java/cn/demo/webmagic/WebMagicDemo.java
+++ src/main/java/cn/demo/webmagic/WebMagicDemo.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDemo {
+public class WebMagicDemo {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemo.class, args);
+		SpringApplication.run(WebMagicDemo.class, args);
 	}
 
 }
